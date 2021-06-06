6dc2fcaf60b8988051bd8c99e041a9077a2f7cba
Mattia Natali <matitalatina@gmail.com>
2021-06-06 13:57:25 +0200

    Added url and token lags

--- main.go
+++ main.go
@@ -1,35 +1,45 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"log"
 	"net/http"
 )
 
-func proxyTorque(w http.ResponseWriter, req *http.Request) {
-	client := &http.Client{}
-	println(req.URL.String())
-	reqHA, err := http.NewRequest(req.Method, "https://xxx"+req.URL.String(), req.Body)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	reqHA.Header.Add("Authorization", "Bearer xxx")
-	resHA, err := client.Do(reqHA)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer resHA.Body.Close()
+func proxyTorque(url string, token string) func (http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		client := &http.Client{}
+		println(req.URL.String())
+		reqHA, err := http.NewRequest(req.Method, url+req.URL.String(), req.Body)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		reqHA.Header.Add("Authorization", "Bearer "+token)
+		resHA, err := client.Do(reqHA)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer resHA.Body.Close()
 
-	for name, values := range resHA.Header {
-		w.Header()[name] = values
+		for name, values := range resHA.Header {
+			w.Header()[name] = values
+		}
+		w.WriteHeader(resHA.StatusCode)
+		io.Copy(w, resHA.Body)
 	}
-	w.WriteHeader(resHA.StatusCode)
-	io.Copy(w, resHA.Body)
 }
 
 func main() {
-	http.HandleFunc("/api/torque", proxyTorque)
+	url := flag.String("url", "", "Home Assistant URL")
+	token := flag.String("token", "", "Home Assistant Long Lived Token")
+	flag.Parse()
+	if *url == "" || *token == "" {
+		flag.PrintDefaults()
+        panic("Please provide both url and token")
+	}
+	http.HandleFunc("/api/torque", proxyTorque(*url, *token))
 	http.ListenAndServe(":8090", nil)
 }
